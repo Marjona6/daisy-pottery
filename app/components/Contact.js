3eaffbdb36ee4f0ce7152f00e413d172af6fbab2
Meryem Yanar <meryem.yanaryanar@gmail.com>
2023-10-21 13:21:34 +0100

    Clean up and style contact page (WIP)

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -27,26 +27,28 @@ export default function Contact() {
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
         <form netlify name="contact" onSubmit={handleSubmit} className="flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <div className="relative mb-4">
-            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+            <label htmlFor="name" className="leading-7 text-sm text-prussian-blue">
               Name
             </label>
-            <input type="text" id="name" name="name" className="w-full rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input type="text" id="name" name="name" className="w-full rounded border border-gray-700 focus:border-verdigris focus:ring-2 focus:ring-verdigris text-base outline-none text-oxford-blue py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
           <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+            <label htmlFor="email" className="leading-7 text-sm text-prussian-blue">
               Email
             </label>
-            <input type="email" id="email" name="email" className="w-full rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input type="email" id="email" name="email" className="w-full rounded border border-gray-700 focus:border-verdigris focus:ring-2 focus:ring-verdigris text-base outline-none text-oxford-blue py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
           <div className="relative mb-4">
-            <label htmlFor="message" className="leading-7 text-sm text-gray-400">
+            <label htmlFor="message" className="leading-7 text-sm text-prussian-blue">
               Message
             </label>
-            <textarea id="message" name="message" className="w-full rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" />
+            <textarea id="message" name="message" className="w-full rounded border border-gray-700 focus:border-verdigris focus:ring-2 focus:ring-verdigris h-32 text-base outline-none text-oxford-blue py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" />
           </div>
-          <button type="submit" className="text-white border-0 py-2 px-6 focus:outline-none rounded text-lg">
-            Submit
-          </button>
+          <div className="w-full flex justify-center">
+            <button type="submit" className="bg-prussian-blue text-white border-0 py-2 px-6 focus:outline-none rounded text-lg w-full md:w-fit">
+              Submit
+            </button>
+          </div>
         </form>
       </div>
     </section>
