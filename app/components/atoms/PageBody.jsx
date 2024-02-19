d00e580e1009de57ba0347b6665ec3cd86103b66
Meryem Yanar <meryem.yanaryanar@gmail.com>
2024-02-19 14:50:11 +0000

    Small aesthetic change to page body component

--- app/components/atoms/PageBody.jsx
+++ app/components/atoms/PageBody.jsx
@@ -1,5 +1,5 @@
 const PageBody = ({ children }) => {
-  return <div className="px-6 py-4 bg-bisque">{children}</div>;
+  return <div className="px-6 py-4 bg-transparent">{children}</div>;
 };
 
 export default PageBody;
