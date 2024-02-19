da74460f7d20e25ddadd13790ef4ae8218772a3e
Meryem Yanar <meryem.yanaryanar@gmail.com>
2024-02-19 14:49:11 +0000

    Update shop page with Facebook link

--- pages/shop.js
+++ pages/shop.js
@@ -1,12 +1,24 @@
-import React from "react";
-import NavBar from "../app/components/NavBar";
-import PageBody from "../app/components/atoms/PageBody";
+import React from 'react';
+import NavBar from '../app/components/NavBar';
+import PageBody from '../app/components/atoms/PageBody';
+
+const facebookUrl = 'https://www.facebook.com/profile.php?id=61554541169488';
 
 export default function Shop() {
   return (
     <main>
       <NavBar />
-      <PageBody>Coming soon!</PageBody>
+      <PageBody>
+        Visit{' '}
+        <a
+          href={facebookUrl}
+          alt="The Little Yorkshire Pottery - Facebook"
+          className="text-verdigris"
+        >
+          The Little Yorkshire Pottery
+        </a>
+        &apos;s shop on Facebook!
+      </PageBody>
     </main>
   );
 }
