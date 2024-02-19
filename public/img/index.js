64c33b5b7eccfe9b80aeab69e26e9c7407818f84
Meryem Yanar <meryem.yanaryanar@gmail.com>
2024-02-19 14:47:20 +0000

    Add images

---
+++ public/img/index.js
@@ -0,0 +1,51 @@
+import img01 from './min/Optimized-DSC03220.jpeg';
+import img02 from './min/Optimized-DSC03241.jpeg';
+import img03 from './min/Optimized-DSC03248.jpeg';
+import img04 from './min/Optimized-DSC03249.jpeg';
+import img05 from './min/Optimized-DSC03290.jpeg';
+import img06 from './min/Optimized-DSC03291.jpeg';
+import img07 from './min/Optimized-DSC03294.jpeg';
+import img08 from './min/Optimized-DSC03306.jpeg';
+import img09 from './min/Optimized-DSC03334.jpeg';
+import img10 from './min/Optimized-DSC03345.jpeg';
+import img11 from './min/Optimized-DSC03354.jpeg';
+import img12 from './min/Optimized-DSC03362.jpeg';
+import img13 from './min/Optimized-DSC03372.jpeg';
+import img14 from './min/Optimized-DSC03377.jpeg';
+import img15 from './min/Optimized-DSC03388.jpeg';
+import img16 from './min/Optimized-DSC03405.jpeg';
+import img17 from './min/Optimized-DSC03414.jpeg';
+import img18 from './min/Optimized-DSC03416.jpeg';
+import img19 from './min/Optimized-DSC03424.jpeg';
+import img20 from './min/Optimized-DSC03429.jpeg';
+import img21 from './min/Optimized-DSC03433.jpeg';
+import img22 from './min/Optimized-DSC03448.jpeg';
+import img23 from './min/Optimized-DSC03463.jpeg';
+import img24 from './min/Optimized-DSC03466.jpeg';
+
+export {
+  img01,
+  img02,
+  img03,
+  img04,
+  img05,
+  img06,
+  img07,
+  img08,
+  img09,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
+  img18,
+  img19,
+  img20,
+  img21,
+  img22,
+  img23,
+  img24,
+};
