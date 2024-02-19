079ffe0e577ccbc223533dee67158155ca662bfd
Meryem Yanar <meryem.yanaryanar@gmail.com>
2024-02-19 14:50:50 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import React from "react";
-import NavBar from "../app/components/NavBar";
-import PageBody from "../app/components/atoms/PageBody";
-import "./globals.css";
-import "tailwindcss/tailwind.css";
+import React from 'react';
+import NavBar from '../app/components/NavBar';
+import PageBody from '../app/components/atoms/PageBody';
+import './globals.css';
+import 'tailwindcss/tailwind.css';
+import Image from 'next/image';
 
 export default function MainPage() {
   return (
@@ -12,11 +13,15 @@ export default function MainPage() {
         className="fixed w-screen h-screen bg-cover h-min-100vh"
         style={{
           backgroundImage: `url('/daisy4.jpeg')`,
-          height: "100%",
-          minHeight: "100vh",
+          height: '100%',
+          minHeight: '100vh',
         }}
       >
-        <PageBody>Main</PageBody>
+        <PageBody>
+          <div className="flex w-full items-center justify-center content-center">
+            {/* <Image src="/logo.svg" alt="The Little Yorkshire Pottery" width={400} height={400} /> */}
+          </div>
+        </PageBody>
       </div>
     </main>
   );
